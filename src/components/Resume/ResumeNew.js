415a3f7f774658a49d61718c2eb4067335315998
ihssane2002 <bammadihssane@gmail.com>
2025-03-26 04:22:05 +0100

    Ajout Resume

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Particle from "../Particle";
+import { Document, Page, pdfjs } from "react-pdf";
+import pdf from "../../Assets/../Assets/cv port.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function ResumeNew() {
+  const [numPages, setNumPages] = useState(null);
+
+  // Called when the PDF is loaded successfully
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+
+        <Row style={{ justifyContent: "center", position: "relative", marginBottom: "20px" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        {/* Display the PDF in the webpage */}
+        <Row style={{ justifyContent: "center", paddingBottom: "20px"}}>
+          <Col md={8}>
+            <Document
+              file={pdf}
+              onLoadSuccess={onDocumentLoadSuccess}
+              className="d-flex justify-content-center"
+            >
+              {/* Render each page of the PDF */}
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </Col>
+        </Row>
+
+        {/* Download button at the bottom */}
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
